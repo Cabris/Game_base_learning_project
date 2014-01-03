bc190f2e91a3c2641f1f3db934c15fdb7a00a450
Cabris <christtabris@gmail.com>
2014-01-03 14:57:45 +0800

    add rotation ani

---
+++ Assets/Scripts/RotationAni.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationAni : MonoBehaviour {
+	public bool x,y,z;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 eulerAngles=new Vector3();
+		if(x)
+			eulerAngles.x=1;
+		if(y)
+			eulerAngles.y=1;
+		if(z)
+			eulerAngles.z=1;
+		transform.Rotate(eulerAngles * Time.deltaTime*speed);
+	}
+}
