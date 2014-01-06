937b321901e080ef93875d8e0d921dcf83066a98
Cabris <christtabris@gmail.com>
2014-01-06 23:04:52 +0800

    在C#裡呼叫js function
    有限制:
    js檔一定要放Assets\Standard Assets目錄中
    我寫了測試用的js檔 testJs1.js
    在Goal.cs裡呼叫，所有js function正常運作

---
+++ Assets/Standard Assets/testJs1.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function testPrint(str,c:int)
+{
+for(var i=0;i<c;i++)
+    print(str+":"+i);
+}
+
+function testPrint2( array:Array)
+{
+for(var i=0;i<array.length;i++){
+ print(i+":"+array[i]);
+}
+}
