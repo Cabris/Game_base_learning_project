c225d6d5ad25eab7a9afc33e99c34fbd5677c054
吳旻杰 <min.chieh.wu@gmail.com>
2014-01-07 22:59:01 +0800

    新增按鍵控制物件旋轉，按左鍵向左轉，按右鍵向右轉。

--- Assets/Scripts/Rotateable.cs
+++ Assets/Scripts/Rotateable.cs
@@ -15,14 +15,26 @@ public class Rotateable : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-	
+				
 		}
 
 		void OnMouseOver ()
 		{
 				if (Enabled)
-						transform.Rotate (Vector3.forward * speed * Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime);//滑鼠滾輪控制旋轉
+				{
+					if(Input.GetKey("left"))
+					{
+						transform.Rotate (Vector3.forward * speed * Time.deltaTime);//左按鍵控制左旋轉
+				 	}
+					else if(Input.GetKey("right"))
+					{
+						transform.Rotate (Vector3.back * speed * Time.deltaTime);//右按鍵控制右旋轉
+					}
+					else
+					{
+						transform.Rotate (Vector3.forward * speed * Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime);//滑鼠滾輪控制旋轉
+					}
+				}
 		}
-
 		public bool Enabled{ get; set; }
 }
