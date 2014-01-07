ab76a6805b08471fcc983e2a9a22b77689932364
Cabris <christtabris@gmail.com>
2014-01-08 02:39:45 +0800

    修正選取物件
    選取物件方式改為用滑鼠點擊
    紅色為被選取狀態
    按空白處或其他物件以取消選取

--- Assets/Scripts/MouseSelect.cs
+++ Assets/Scripts/MouseSelect.cs
@@ -1,23 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 [RequireComponent(typeof(Renderer))]
 public class MouseSelect : MonoBehaviour {
 	public bool dragable,rotateable;
 	Color SelectingColor=Color.red;
 	Color tempColor;
-	public Renderer _rernderer;
+	public Renderer subRernderer;
 	Dragable _dragable;
 	Rotateable _rotateable;
+	public SelectionState selectionState;
+	public bool isHover;//滑鼠是否移上去
+
 	// Use this for initialization
 	void Start () {
 		_dragable=GetComponent<Dragable>();
 		_rotateable=GetComponent<Rotateable>();
-		//		if(_rotateable!=null)
-		//			rotateable=_rotateable.Enabled;
-		//		if(_dragable!=null)
-		//			dragable=_dragable.Enabled;
-		
-		
+		selectionState=SelectionState.NotSelect;
+		isHover=false;
+
 	}
 	
 	// Update is called once per frame
@@ -26,25 +28,50 @@ public class MouseSelect : MonoBehaviour {
 			_rotateable.Enabled=rotateable;
 		if(_dragable!=null)
 			_dragable.Enabled=dragable;
+		upDateMaterialStaus();
+		
+		if (Input.GetMouseButtonDown(0)&&!isHover||Input.GetKey(KeyCode.Escape)){//
+			selectionState=SelectionState.NotSelect;
+		}
+		
+	}
+	
+	void upDateMaterialStaus(){
+		if(selectionState==SelectionState.Selected){
+			if(subRernderer!=null){
+				tempColor=subRernderer.material.color;
+				subRernderer.material.color =SelectingColor;
+			}
+			if(renderer!=null){
+				renderer.material.color =SelectingColor;
+			}
+		}if(selectionState==SelectionState.NotSelect){
+			if(subRernderer!=null)
+				subRernderer.material.color = tempColor;
+			if(renderer!=null)
+				renderer.material.color = Color.white;
+		}
+	}
+
+	void OnMouseDown() {
+		selectionState=SelectionState.Selected;
+	}
+	
+	void OnMouseUp() {
+		//selectionState=SelectionState.NotSelect;
 	}
 	
 	void OnMouseEnter() {
-		
-		if(_rernderer!=null){
-			tempColor=_rernderer.material.color;
-			_rernderer.material.color =SelectingColor;
-		}
-		if(renderer!=null){
-			renderer.material.color =SelectingColor;
-		}
+		isHover=true;
 	}
 	
 	void OnMouseExit() {
-		if(_rernderer!=null)
-			_rernderer.material.color = tempColor;
-		
-		if(renderer!=null)
-			renderer.material.color = Color.white;
+		isHover=false;
 	}
+	
+	public enum SelectionState{
+		NotSelect,//未選取
+		Selected//選取
+	};
 	
 }
--- Assets/Scripts/Rotateable.cs
+++ Assets/Scripts/Rotateable.cs
@@ -3,38 +3,46 @@ using System.Collections;
 
 public class Rotateable : MonoBehaviour
 {
-
-		public float speed;
-
-		// Use this for initialization
-		void Start ()
-		{
-				Enabled = true;
-		}
+	public float speed;
+	MouseSelect selection;
 	
-		// Update is called once per frame
-		void Update ()
-		{
-				
+	// Use this for initialization
+	void Start ()
+	{
+		Enabled = true;
+		selection=GetComponent<MouseSelect>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(selection!=null&&selection.selectionState==MouseSelect.SelectionState.Selected)
+			UpdateRotate ();
+	}
+	
+	public void UpdateRotate ()
+	{
+		if (Enabled) {
+			if (Input.GetKey ("left")) {
+				transform.Rotate (Vector3.forward * speed * Time.deltaTime);
+				//左按鍵控制左旋轉
+			}
+			else
+			if (Input.GetKey ("right")) {
+				transform.Rotate (Vector3.back * speed * Time.deltaTime);
+				//右按鍵控制右旋轉
+			}
+			else {
+				transform.Rotate (Vector3.forward * speed * Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime);
+				//滑鼠滾輪控制旋轉
+			}
 		}
-
-		void OnMouseOver ()
-		{
-				if (Enabled)
-				{
-					if(Input.GetKey("left"))
-					{
-						transform.Rotate (Vector3.forward * speed * Time.deltaTime);//左按鍵控制左旋轉
-				 	}
-					else if(Input.GetKey("right"))
-					{
-						transform.Rotate (Vector3.back * speed * Time.deltaTime);//右按鍵控制右旋轉
-					}
-					else
-					{
-						transform.Rotate (Vector3.forward * speed * Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime);//滑鼠滾輪控制旋轉
-					}
-				}
-		}
-		public bool Enabled{ get; set; }
+	}
+	
+	void OnMouseOver ()
+	{
+		if(selection==null)
+			UpdateRotate ();
+	}
+	public bool Enabled{ get; set; }
 }
