f3edec8c7f64dd781bff95e1d64551e7ed72f492
吳旻杰 <min.chieh.wu@gmail.com>
2014-01-08 22:05:58 +0800

    寫了一個使Script能夠觸發PlayMaker上面之Event的範例。

---
+++ Assets/ScriptConnPlayMaker.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var TestFSM : PlayMakerFSM;
+TestFSM = gameObject.Find("Main Camera").GetComponent.<PlayMakerFSM>(); //尋找Main Camera上的PlayMaker
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI ()
+{
+	if(GUI.Button (Rect(295, 145,75,40),"Test"))
+	{
+		TestFSM.Fsm.Event("Start an Event"); //執行Main Camera上的PlayMaker之Start an Event事件
+	}
+}
