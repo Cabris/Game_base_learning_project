9b38d809729a5a44783563e3774d64cd63a629dc
Cabris <christtabris@gmail.com>
2014-01-10 04:54:51 +0800

    用ngui重做button
    原本的button砍掉了

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour {
+	public PlayMakerFSM fsm;
+	public string _event="";
+	// Use this for initialization
+	void Start () {
+		if(fsm==null)fsm=Camera.main.GetComponent<PlayMakerFSM>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick ()
+	{
+		fsm.SendEvent(_event);
+	}
+}
